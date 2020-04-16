27fe1bb063c959e542f05365a9d465b8f892a02d
Tiberio Brasil <tiberiobrasil@gmail.com>
2020-04-15 21:05:38 -0300

    Finalizado codigo do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,19 @@
 import Transaction from '../models/Transaction';
 
+interface ReturnDataDTO {
+  transactions: Transaction[];
+
+  balance: Balance;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -13,16 +27,41 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): ReturnDataDTO {
+    const returnData: ReturnDataDTO = {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
+
+    return returnData;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+
+    // eslint-disable-next-line no-return-assign
+    this.transactions.map(transaction =>
+      transaction.type === 'income'
+        ? (income += transaction.value)
+        : (outcome += transaction.value),
+    );
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
